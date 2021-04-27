b346633892011a888f801db774057b7b6e1ee0e8
qhsgho <45326933+qhsgho@users.noreply.github.com>
2021-04-28 02:50:25 +0900

    MonthViewFragment 생성

--- app/src/main/java/com/android/androidproject2/MainActivity.java
+++ app/src/main/java/com/android/androidproject2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.android.androidproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_container, new MonthViewFragment());
+        fragmentTransaction.commit();
     }
 
     @Override
