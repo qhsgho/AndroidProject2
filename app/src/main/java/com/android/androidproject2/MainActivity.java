f7b497045a2a569b48fc7916b3437969f7201abb
qhsgho <45326933+qhsgho@users.noreply.github.com>
2021-04-28 02:37:09 +0900

    앱바에 오버플로우 메뉴 추가
    월
    주

--- app/src/main/java/com/android/androidproject2/MainActivity.java
+++ app/src/main/java/com/android/androidproject2/MainActivity.java
@@ -3,6 +3,10 @@ package com.android.androidproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.month_view:
+                Toast.makeText(getApplicationContext(), "month_view", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.week_view:
+                Toast.makeText(getApplicationContext(), "week_view", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
